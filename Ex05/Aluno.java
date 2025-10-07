17e7577e22dcd091af13345773743bdb586660cc
LFMineiro <lflmalbuquerque3@gmail.com>
2025-10-07 18:20:58 -0300

    finish overriding equals method for student comparison

--- Ex05/Aluno.java
+++ Ex05/Aluno.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Aluno {
     private String nome;
     private Integer matricula;
@@ -10,6 +12,15 @@ public class Aluno {
     @Override
     public String toString() {
         return "Aluno: " + nome + " (" + matricula + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        Aluno aluno = (Aluno) obj;
+        return Objects.equals(matricula, aluno.matricula);
     }
     
     
--- Ex05/Main.java
+++ Ex05/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno("Luiz", 23048);
+        Aluno aluno2 = new Aluno("Thiago", 23048);
         System.out.println(aluno1);
+        System.out.println(aluno2);
+        System.out.println(aluno1.equals(aluno2));
     }
 }
